e4b3e921c2548314067061d3b6b0d9d485972a95
Andrey Simurzin <asimurzin@gmail.com>
2011-12-29 09:21:41 +0300

    porting on 2.1.0

--- rhoPorousMRFPimpleFlux/__init__.py
+++ rhoPorousMRFPimpleFlux/__init__.py
@@ -39,6 +39,13 @@ if FOAM_REF_VERSION( ">=", "020000" ):
     
 
 #--------------------------------------------------------------------------------------
+from Foam import FOAM_REF_VERSION
+if FOAM_REF_VERSION( ">=", "020100" ):
+    from rhoPorousMRFPimpleFlux.r2_1_0 import *
+    pass
+    
+
+#--------------------------------------------------------------------------------------
 def entry_point():
     try:
        engine = main_standalone       
